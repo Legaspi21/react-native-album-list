7043d35e32b04cbf3e8373a840be92c8a5ae58ff
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-21 11:08:25 -0800

    First component

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,10 @@ import React from 'react';
 import ReactNative from 'react-native';
 
 // Create a component
-
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+    );
+};
 // Render it to the device
 
