9cfbe0ba353078281b075894015b6af42dc4c5b0
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-21 11:01:13 -0800

    Import React and React-Native

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// Index.ios.js - place code in here for IOS!!!
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+// Render it to the device
+
